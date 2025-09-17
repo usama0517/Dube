4af4a7a206500de30647e303d21470d90f36f135
Usama Abdurahman <usamaabdurahman05@gmail.com>
2025-09-17 19:25:23 +0300

    Fixing to build

--- Frontend/src/Pages/Home/ViewProfiles.tsx
+++ Frontend/src/Pages/Home/ViewProfiles.tsx
@@ -1,26 +1,32 @@
+import { NavLink } from 'react-router-dom'
 import './viewProfile.css'
 
 export default function ViewProfiles() {
   return (
   <>
+        <NavLink style={{margin:"10px"}} to={"/credit/1"}>Credit Detail </NavLink>
+        <NavLink style={{margin:"10px"}} to={"/create/profile"}>New Profile </NavLink>
+          <NavLink style={{margin:"10px"}} to={"/create/credit"}>New Credit </NavLink>
+          <NavLink style={{margin:"10px"}} to={"/edit/credit"}>Edit Credit </NavLink>
+          <NavLink style={{margin:"10px"}} to={"/edit/profile"}>Edit profile </NavLink>
    
     <div className="top-button-holder">
       <button >+ New Profile</button>
       </div>
-           
+                   
+      <div className="card">
+        
+        <h3>Usama </h3>
+        <h3>Abdurahman</h3>
+      </div>
+        
       <div className="card">
         
         <h3>Usama </h3>
         <h3>Abdurahman</h3>
       </div>
 
-      <div className="card">
         
-        <h3>Usama </h3>
-        <h3>Abdurahman</h3>
-      </div>
-
-
       <div className="card">
         
         <h3>Usama </h3>
@@ -29,13 +35,13 @@ export default function ViewProfiles() {
       
        
 
-        
+                
       <div className="card">
         
         <h3>Usama </h3>
         <h3>Abdurahman</h3>
       </div>
-
+        
       <div className="card">
         
         <h3>Usama </h3>
