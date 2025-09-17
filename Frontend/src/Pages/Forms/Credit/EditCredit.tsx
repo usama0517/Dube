269aa0aa6ef704f56244a6e0052bbe76ffcce5a1
Usama Abdurahman <usamaabdurahman05@gmail.com>
2025-09-17 18:37:16 +0300

    Fixing to build

--- Frontend/src/Pages/Forms/Credit/EditCredit.tsx
+++ Frontend/src/Pages/Forms/Credit/EditCredit.tsx
@@ -9,7 +9,7 @@ export default function EditCredit() {
        </div>
        
     <div className='form-container'>
-      <form onSubmit={(e)=>{submitter(e.nativeEvent as SubmitEvent)}}>
+      <form>
         <div className='label-holder'>
         <label>Item</label>
              <input name='fname' type='text' placeholder='Please Enter Item' required />
@@ -41,8 +41,6 @@ export default function EditCredit() {
 
    
   ) 
-  function submitter(e: SubmitEvent) {
-  e.preventDefault();
-  // logic here
-}
+
+
 }
